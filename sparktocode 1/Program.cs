fae472c6b8f5012637a57f6b2f1cfdf872f723dc
Ahad7705 <22F22914@mec.edu.om>
2026-07-01 23:33:52 +0400

    implemented problem 10(implemented problem )

--- sparktocode 1/Program.cs
+++ sparktocode 1/Program.cs
@@ -190,7 +190,7 @@
 
             ////////////////////////////////////////////////////////////////////////
             //Task 9 - Day Name Finder
-            
+
             //int day;
 
             //Console.WriteLine("Enter day number (1-7):");
@@ -222,6 +222,67 @@
             //    default:
             //        Console.WriteLine("Invalid day number");
             //        break;
+
+            ///////////////////////////////////////////////////
+            //Task 10 - Mini Calculator
+
+            //    double num1, num2, result;
+            //    char operation;
+
+            //    Console.WriteLine("Enter first number:");
+            //    num1 = Convert.ToDouble(Console.ReadLine());
+
+            //    Console.WriteLine("Enter second number:");
+            //    num2 = Convert.ToDouble(Console.ReadLine());
+
+            //    Console.WriteLine("Enter operation (+, -, , /, %):");
+            //    operation = Convert.ToChar(Console.ReadLine());
+
+            //    switch (operation)
+            //    {
+            //        case '+':
+            //            result = num1 + num2;
+            //            Console.WriteLine("Result: " + result);
+            //            break;
+
+            //        case '-':
+            //            result = num1 - num2;
+            //            Console.WriteLine("Result: " + result);
+            //            break;
+
+            //        case '*':
+            //            result = num1 * num2;
+            //            Console.WriteLine("Result: " + result);
+            //            break;
+
+            //        case '/':
+            //            if (num2 != 0)
+            //            {
+            //                result = num1 / num2;
+            //                Console.WriteLine("Result: " + result);
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("Cannot divide by zero");
+            //            }
+            //            break;
+
+            //        case '%':
+            //            if (num2 != 0)
+            //            {
+            //                result = num1 % num2;
+            //                Console.WriteLine("Result: " + result);
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("Cannot divide by zero");
+            //            }
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("Invalid operator");
+            //            break;
+            //    }
 
 
         }
