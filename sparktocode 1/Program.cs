07ee5c4772da971255f99ce790e036704a70699a
Ahad7705 <22F22914@mec.edu.om>
2026-07-01 22:08:24 +0400

    implemented problem 1(Personal Info Card)

---
+++ sparktocode 1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace sparktocode_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 - Personal Info Card
+            //string name = "Ahad";
+            //int age = 20;
+            //double height = 1.75;
+            //bool isStudent = true;
+
+            //Console.WriteLine("Name: " + name);
+            //Console.WriteLine("Age: " + age);
+            //Console.WriteLine("Height: " + height);
+            //Console.WriteLine("Student: " + isStudent);
+
+            //Console.WriteLine("------------------");
+
+            /////////////////////////////////////////////////////
+        }
+    }
+}
