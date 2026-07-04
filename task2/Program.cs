d4bcd85abff64a46988c026b9bbbdd1fe046f0b8
Ahad7705 <22F22914@mec.edu.om>
2026-07-04 11:00:21 +0400

    implement problem 1(Countdown Timer)

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1 - Countdown Timer
+
+            //int number;
+
+            //Console.WriteLine("Enter a starting number:");
+            //number = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = number; i >= 1; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine("Liftoff!");
+            ////////////////////////////////////////////////
+        }
+    }
+}
