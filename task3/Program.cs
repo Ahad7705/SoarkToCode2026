6d28f9d79142e80604cf1d42ad01457a803c8def
Ahad7705 <ii2804679@gmail.com>
2026-07-05 21:15:24 +0400

    implement problem 1(Absolute Difference)

---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1 - Absolute Difference
+
+            //double num1;
+            //double num2;
+            //double difference;
+
+            //Console.WriteLine("Enter first number:");
+            //num1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Enter second number:");
+            //num2 = Convert.ToDouble(Console.ReadLine());
+
+            //difference = Math.Abs(num1 - num2);
+
+            //Console.WriteLine("Positive Difference: " + difference);
+            //////////////////////////////////////////////////////////////////
+            
+
+        }
+    }
+}
