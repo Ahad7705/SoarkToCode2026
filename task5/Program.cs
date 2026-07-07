0bed30bdbf4aba5b96d48ef2fc039b4a1243616d
Ahad7705 <ii2804679@gmail.com>
2026-07-07 22:33:10 +0400

    implement problem 1(Fixed Grades Array)

---
+++ task5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1 - Fixed Grades Array
+
+            int[] grades = new int[5];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine("Enter grade:");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Student Grades:");
+
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
