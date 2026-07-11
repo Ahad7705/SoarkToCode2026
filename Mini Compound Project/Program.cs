d7c70b48e73628065954269a44bea38d69a6f39f
Ahad7705 <ii2804679@gmail.com>
2026-07-11 21:10:30 +0400

    Create banking project structure

---
+++ Mini Compound Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiniCompoundProject
+{
+    internal class Program
+    {
+        static List<string> customerNames = new List<string>();
+        static List<string> accountNumbers = new List<string>();
+        static List<double> balances = new List<double>();
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void AddAccount()
+        {
+
+        }
+    }
+}
