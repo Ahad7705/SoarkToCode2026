221f9b5adc82ebd65dd473a5442b8ea57959fe97
Ahad7705 <ii2804679@gmail.com>
2026-07-11 21:50:12 +0400

    Implement banking system menu

--- Mini Compound Project/Program.cs
+++ Mini Compound Project/Program.cs
@@ -11,6 +11,57 @@ namespace MiniCompoundProject
 
         static void Main(string[] args)
         {
+            bool exitApp = false;
+
+            while (!exitApp)
+            {
+                Console.WriteLine("\n===== Welcome to Spark Bank =====");
+                Console.WriteLine("1. Add New Account");
+                Console.WriteLine("2. Deposit Money");
+                Console.WriteLine("3. Withdraw Money");
+                Console.WriteLine("4. Show Balance");
+                Console.WriteLine("5. Transfer Amount");
+                Console.WriteLine("6. List All Accounts");
+                Console.WriteLine("7. Find Richest Customer");
+                Console.WriteLine("8. Exit");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        AddAccount();
+                        break;
+
+                    case 2:
+                        DepositMoney();
+                        break;
+
+                    case 3:
+                        WithdrawMoney();
+                        break;
+
+                    case 4:
+                        ShowBalance();
+                        break;
+
+                    case 5:
+                        TransferAmount();
+                        break;
+
+                    case 6:
+                        ListAllAccounts();
+                        break;
+
+                    case 7:
+                        FindRichestCustomer();
+                        break;
+
+                    case 8:
+                        exitApp = true;
+                        break;
+                }
+            }
 
         }
 
