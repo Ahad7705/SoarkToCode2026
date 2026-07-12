5565bd80ec9ddf4e3815e87da1cb47565b6c0218
Ahad7705 <ii2804679@gmail.com>
2026-07-12 22:23:19 +0400

    implement product class

---
+++ Part 1 Bank Student & Product Management/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Part_1_Bank_Student_Product_Management
+{
+    public class BankAccount
+    {
+        
+    }
+
+    public class Student
+    {
+        
+    }
+
+    
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int StockQuantity { get; set; }
+
+        public void Sell(int quantity)
+        {
+            if (StockQuantity >= quantity)
+            {
+                StockQuantity -= quantity;
+            }
+            else
+            {
+                Console.WriteLine("Not enough stock.");
+            }
+
+            LogTransaction();
+        }
+
+        public void Restock(int quantity)
+        {
+            StockQuantity += quantity;
+            LogTransaction();
+        }
+
+        public double GetInventoryValue()
+        {
+            PrintDetails();
+            return Price * StockQuantity;
+        }
+
+        private void PrintDetails()
+        {
+            Console.WriteLine("Product Name: " + ProductName);
+            Console.WriteLine("Price: " + Price);
+            Console.WriteLine("Stock Quantity: " + StockQuantity);
+        }
+
+        private void LogTransaction()
+        {
+
+        }
+    }
+
+     
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
