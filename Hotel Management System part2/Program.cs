76cf268c23a44e16331bc7a584fc69bb26e5746b
Ahad7705 <ii2804679@gmail.com>
2026-07-15 20:19:48 +0400

    Create Room and Guest classes

---
+++ Hotel Management System part2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Hotel_Management_System_part2
+{
+    public class Room
+    {
+        public int RoomNumber { get; set; }
+        public string RoomType { get; set; }
+        public double PricePerNight { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public void DisplayRoom()
+        {
+            Console.WriteLine("Room Number: " + RoomNumber);
+            Console.WriteLine("Room Type: " + RoomType);
+            Console.WriteLine("Price Per Night: " + PricePerNight);
+            Console.WriteLine("Available: " + IsAvailable);
+        }
+    }
+    public class Guest
+    {
+        public string GuestId { get; set; }
+        public string GuestName { get; set; }
+        public string RoomNumber { get; set; }
+        public string CheckInDate { get; set; }
+        public int TotalNights { get; set; }
+
+        public void DisplayGuest()
+        {
+            Console.WriteLine("Guest ID: " + GuestId);
+            Console.WriteLine("Guest Name: " + GuestName);
+        }
+
+        public double CalculateTotalCost(double pricePerNight)
+        {
+            return pricePerNight * TotalNights;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
