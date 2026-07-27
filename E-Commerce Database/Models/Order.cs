89ef2b91f60d013f0d92f2699497eeda1d6ae790
Ahad7705 <ii2804679@gmail.com>
2026-07-27 20:50:52 +0400

    Implement  Order and Review class

---
+++ E-Commerce Database/Models/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce_Database.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+
+        // User 1 : M Order
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+
+        // Order M : M Product
+
+        public List<OrderProduct> OrderProducts { get; set; }
+
+
+        // Order 1 : 1 Review
+
+        public Review Review { get; set; }
+    }
+}
+
---
+++ E-Commerce Database/Models/Review.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce_Database.Models
+{
+    internal class Review
+    {
+        using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerceApp.Models
+    {
+        public class Review
+        {
+            public int ReviewId { get; set; }
+
+            public int Rating { get; set; }
+
+            public string Comment { get; set; }
+
+
+            // Order 1 : 1 Review
+
+            [ForeignKey("Order")]
+            public int OrderId { get; set; }
+
+            public Order Order { get; set; }
+        }
+    }
+}
+}
