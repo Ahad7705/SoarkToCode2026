61e435e14b3965bcfc62c0512a4cc38a1a2f1067
Ahad7705 <ii2804679@gmail.com>
2026-07-27 21:27:33 +0400

    Add the AppDbContext

---
+++ E-Commerce Database/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using E_Commerce_Database.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce_Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                @"Server=.\SQLEXPRESS;Database=ECommerceDB;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderProduct>()
+                .HasKey(op => new { op.OrderId, op.ProductId });
+        }
+
+    }
+}
